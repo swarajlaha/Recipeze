2fa46c36d43bdbe02dd5bc0e80354bd6168b4ce5
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-08-21 12:30:51 +0530

    Added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -6,4 +7,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 };
