fefe7bcd12c2edde1451732a577b1b6b272836f3
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-08-22 13:01:28 +0530

    Added event listener and controller for search

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 /** Global state of the app
  * - Search ojc
  * - Current recipe obj
@@ -9,22 +11,25 @@ const state = {};
 
 const controlSearch = async() => {
     // 1. Get query from the view.
-    const query = 'pizza'; // TODO
+    const query = searchView.getInput(); // TODO
+    console.log(query);
     if (query) {
         // 2) New state obj and add to state.
         state.search = new Search(query);
 
         // 3) Prepare UI for results.
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4) Search for recipes.
         await state.search.getResults();
 
         // 5) Render results on UI.
-        console.log(state.search.results);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
