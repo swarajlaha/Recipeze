e8b26baea3be7873d40f8831676c16879cf0d815
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-08-26 11:08:41 +0530

    Modified UI results and imoorted renderLoader

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader } from './views/base';
 /** Global state of the app
  * - Search ojc
  * - Current recipe obj
@@ -20,11 +20,13 @@ const controlSearch = async() => {
         // 3) Prepare UI for results.
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes.
         await state.search.getResults();
 
         // 5) Render results on UI.
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
