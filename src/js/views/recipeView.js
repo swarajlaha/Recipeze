f8d01f1dde4fcb79d474d203b2a9a6e560025066
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-08-29 14:39:35 +0530

    Likes model

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -35,7 +35,7 @@ const createIngredient = ingredient => `
         </div>
     </li>
 `;
-export renderRecipe = recipe => {
+export const renderRecipe = (recipe, isLiked) => {
     const markup = `
         <figure class="recipe__fig">
         <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
@@ -74,7 +74,7 @@ export renderRecipe = recipe => {
             </div>
             <button class="recipe__love">
                 <svg class="header__likes">
-                    <use href="img/icons.svg#icon-heart-outlined"></use>
+                    <use href="img/icons.svg#icon-heart-hearts${isLiked ? '' : '-outlined'}"></use>
                 </svg>
             </button>
         </div>
